d9ee16263cae37569cf18e232ca5f4df0844d641
Raul Martinez <raulmartinez1855@gmail.com>
2020-06-20 13:29:11 -0400

    loading state

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import useCountDown from "../hooks/useCountDown";
 import AudioPlayer from "./AudioPlayer";
 
@@ -14,18 +14,56 @@ const LoadingContainer = styled.div`
   color: ${({ countdown }) => (countdown % 2 !== 0 ? "white" : "black")};
 `;
 
+const grow = keyframes`
+  from {
+    transform: scale(.8);
+  }
+
+  to {
+    transform: scale(1);
+  }
+`;
+
+const LargeNumber = styled.h1`
+  font-size: 10rem;
+  animation: ${grow} 90ms ease-out;
+`;
+
 export default function Loading({ setGameStart }) {
   const countdown = useCountDown(3);
   useEffect(() => {
     if (countdown === -1) {
       setGameStart();
     }
-  }, [countdown, setGameStart]);
+  });
 
+  // messy I know, was having an issue where audio wasnt changing, quick fix for now
   return (
     <LoadingContainer countdown={countdown}>
-      <h1>{!!countdown ? countdown : "GO!"}</h1>
-      {/* <AudioPlayer source={countdown} /> */}
+      {countdown === 3 && (
+        <>
+          <LargeNumber>{countdown}</LargeNumber>
+          <AudioPlayer source={countdown} />
+        </>
+      )}
+      {countdown === 2 && (
+        <>
+          <LargeNumber>{countdown}</LargeNumber>
+          <AudioPlayer source={countdown} />
+        </>
+      )}
+      {countdown === 1 && (
+        <>
+          <LargeNumber>{countdown}</LargeNumber>
+          <AudioPlayer source={countdown} />
+        </>
+      )}
+      {countdown === 0 && (
+        <>
+          <LargeNumber>GO!</LargeNumber>
+          <AudioPlayer source={countdown} />
+        </>
+      )}
     </LoadingContainer>
   );
 }
